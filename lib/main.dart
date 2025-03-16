7a8306fc438e2c0bfac9349bf0bedfdb0c0891be
Yibran Manuel <yibran.perve@gmail.com>
2025-03-15 22:13:21 -0600

    add page welcome

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:inright/features/home/presentation/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:inright/features/home/presentation/home.dart';
+import 'package:inright/pages/welcome/welcome_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isFirstTime = prefs.getBool('first_time') ?? true;
+
+  runApp(MainApp(isFirstTime: isFirstTime));
+
+  if (isFirstTime) {
+    await prefs.setBool('first_time', false);
+  }
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final bool isFirstTime;
+
+  const MainApp({super.key, required this.isFirstTime});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-      routes: {'/home': (context) => Home()},
+      initialRoute: isFirstTime ? '/welcome' : '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/welcome': (context) => WelcomeScreen(),
+      },
     );
   }
 }
